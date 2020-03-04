4250e7d629dfebad3604f5480746f9692bdab914
Florin Mihalache <florinrmfm3@gmail.com>
2020-03-04 22:31:48 +0200

    Deleted useless comments and prints

--- client/client.py
+++ client/client.py
@@ -7,14 +7,6 @@ app = Flask(__name__)
 app.static_folder = 'static'
 
 
-'''
-@app.route('/stylesheets/<path:filename>')
-def serve_static(filename):
-    root_dir = os.path.dirname(os.getcwd())
-    return send_from_directory(os.path.join(root_dir, 'static', 'js'), filename)
-'''
-
-
 @app.route('/')
 def index():
     template = render_template('index.html')
@@ -23,7 +15,6 @@ def index():
 
 @app.route('/page/')
 def page():
-    print('sex anal cu mama lui costi care e curva mare')
     template = render_template('page.html')
     return template
 
